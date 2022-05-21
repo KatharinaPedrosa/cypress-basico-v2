992e7ca24d5a5e4c88378df98eee78cebe62e2a8
Katharina <katharinaandressa@hotmail.com>
2022-05-20 22:16:59 -0300

    marcando elementos em tela

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,7 +15,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName').type('Pedrosa')
         cy.get('#email').type('katharinapedrosa@teste.com')
         cy.get('#open-text-area').type(longText, { delay: 0 })
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     })
 
@@ -24,7 +24,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName').type('Pedrosa')
         cy.get('#email').type('katharinapedrosa@teste,com')
         cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
 
@@ -41,7 +41,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#email').type('katharinapedrosa@teste.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
 
     })
@@ -69,10 +69,53 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', '')
 
     })
-    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',function(){
-        cy.get('button[type="submit"]').click()
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
+    it('envia o formuário com sucesso usando um comando customizado', function () {
+        cy.fillMandatoryFieldsAndSubmit()
+        cy.get('.success').should('be.visible')
+    })
+    it('seleciona um produto aleatoriamente', function () {
+        cy.get('select option')
+            .its('length', { log: false }).then(n => {
+                cy.get('select').select(Cypress._.random(n - 1))
+            })
+    })
 
+    it('seleciona um produto (YouTube) por seu texto', function () {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+    it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+    it('seleciona um produto (Blog) por seu índice', function () {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+    it('marca o tipo de atendimento "Feedback"', function () {
+        cy.get('input[type="radio"]')
+            .check('feedback')
+            .should('have.value', 'feedback')
+
+    })
+    it.only('marca cada tipo de atendimento',function(){
+        cy.get('input[type="radio"]')
+            .should('have.length',3)
+            .each(function($radio){
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
+    })
 
 })
+
+
+
+
